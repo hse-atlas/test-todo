844272f2d24441f4b53b058ec1320dfaa31fd428
Чеченов Роберт Адамович <rachechenov@edu.hse.ru>
2025-03-26 19:59:50 +0300

    настройка портов и версий докера

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Переходим в директорию проекта
-cd /путь/к/директории/test-todo
-
 # Останавливаем существующие контейнеры, если они есть
 docker-compose down
 
-# Собираем и запускаем контейнеры
-docker-compose up -d
+# Собираем и запускаем контейнеры с перестройкой
+docker-compose up -d --build
 
 # Проверяем статус
 echo "Статус контейнеров:"
 docker-compose ps
 
 echo "Приложение доступно по адресам:"
-echo "- Frontend: http://YOUR_SERVER_IP:3001"
-echo "- Backend API: http://YOUR_SERVER_IP:8080"
-echo "- Swagger Docs: http://YOUR_SERVER_IP:8080/docs"
+echo "- Frontend: http://$(hostname -I | awk '{print $1}'):3456"
+echo "- Backend API: http://$(hostname -I | awk '{print $1}'):8888"
+echo "- Swagger Docs: http://$(hostname -I | awk '{print $1}'):8888/docs"
